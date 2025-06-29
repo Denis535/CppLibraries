d9a012cecd30ded00bcfb2ef6a0320cf66987a1d
Denis <dddenisss35@gmail.com>
2025-06-29 21:20:49 +0300

    Stateful.Pro

---
+++ Stateful.Pro/Stateful.Pro.Tests/Sources/Stateful.Hierarchical/Tests_00.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "Stateful.h"
+
+namespace Stateful.Hierarchical {
+    using namespace std;
+
+    TEST(Tests_00, Test_00) { // NOLINT
+
+    }
+
+}
---
+++ Stateful.Pro/Stateful.Pro.Tests/Sources/Stateful/Tests_00.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+#include "Stateful.h"
+
+namespace Stateful {
+    using namespace std;
+
+    //     TEST(Tests_00, Test) { // NOLINT
+    // #if defined(__clang__) && !defined(_MSC_VER)
+    //         SUCCEED() << "Compiler: Clang" << endl;
+    // #elif defined(__clang__) && defined(_MSC_VER)
+    //         SUCCEED() << "Compiler: Clang-cl (Clang with MSVC compatibility)" << endl;
+    // #elif defined(_MSC_VER)
+    //         SUCCEED() << "Compiler: MSVC" << endl;
+    // #elif defined(__GNUC__)
+    //         SUCCEED() << "Compiler: GCC" << endl;
+    // #else
+    //         SUCCEED() << "Compiler: Unknown" << endl;
+    // #endif
+    //     }
+
+    TEST(Tests_00, Test_00) { // NOLINT
+
+    }
+
+}
